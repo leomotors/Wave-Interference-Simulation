Correct the formula

main.py
@@ -7,6 +7,8 @@ import math
 
 FRAMES = int(input("Frames Count: "))
 
+SPREADING = int(input("[Wave Config] Spreading: "))
+
 TICK_RATE = 0.1
 
 IMG_HEIGHT, IMG_WIDTH = (int(k) for k in input("Height, Width: ").split())
@@ -25,23 +27,24 @@ for wave in range(WAVE_COUNT):
     CurrWaveObj.setPhase(cwp[3])
     List_Of_Wave.append(CurrWaveObj)
 
-print("Processing Wave...")
+fname = input("Please enter output file name: ")
 
+
+print("Processing Wave...")
 Wave_F1 = []
 Wave_F2 = []
 for wave in ProgressBar(List_Of_Wave):
-    Wave_F1.append(wave.getWaveFunction())
+    Wave_F1.append(wave.getWaveFunction(SPREADING))
     Wave_F2.append(wave.distanceFunction())
 
-print("Image Processing...")
+print("\nImage Processing...")
 images = []
 for frame in ProgressBar(range(FRAMES)):
     Picture_Array = np.zeros((IMG_WIDTH, IMG_HEIGHT), np.uint8)
-    for pixel in range(IMG_HEIGHT*IMG_WIDTH):
-        x = math.floor(pixel / IMG_HEIGHT)
-        y = pixel % IMG_WIDTH
-
-        thisAmplitude = 0
+    for pixel in ProgressBar(range(IMG_WIDTH*IMG_HEIGHT), leave=False):
+        x = math.floor(pixel/IMG_HEIGHT)
+        y = pixel % IMG_HEIGHT
+        thisAmplitude = 127
         for wind in range(WAVE_COUNT):
             subWaveAmp = Wave_F1[wind](Wave_F2[wind](x, y), TICK_RATE * frame)
             thisAmplitude += subWaveAmp
@@ -49,7 +52,7 @@ for frame in ProgressBar(range(FRAMES)):
     Image_Obj = Image.fromarray(Picture_Array)
     images.append(Image_Obj)
 
-fname = input("Image Processing done, enter file name: ")
-
+print("\nImage Processing done. Saving files...")
 images[0].save("./generated/{}.gif".format(fname),
                save_all=True, append_images=images[1:])
+print("Saving DONE!")